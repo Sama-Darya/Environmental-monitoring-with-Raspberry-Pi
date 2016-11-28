49b006529da361e6c4f00e69115bf802c2a025da
treasurenkawu16 <t.m.nkawu@hotmail.co.uk>
2016-11-28 22:19:51 +0000

    Adding Circular bufer final files

---
+++ circularbuffer.cpp
@@ -0,0 +1,32 @@
+//
+//  circularbuffer.cpp
+//  Circularbuffer
+//
+//  Created by Treasure Nkawu on 16/04/2017.
+//  Copyright © 2017 Treasure Nkawu. All rights reserved.
+//
+
+#include "circularbuffer.h"
+
+//Constructor
+Circularbuffer::Circularbuffer(int s) {
+        cap= s; //capacity
+        queue = new double[cap];
+        head = 0; tail = 0;
+        buffer_counter = 0;
+}
+Circularbuffer::~Circularbuffer() { delete [] queue; }
+void Circularbuffer::Insert(int v) {
+        queue[tail] = v;
+        tail = (tail+1) % cap;
+        buffer_counter++;
+    }
+int Circularbuffer::Remove() {
+    /* Return data before removing it from the buffer. */
+        int t = queue[head];
+        head = (head + 1) % cap;
+        return t;
+    }
+int Circularbuffer::Empty() {return (head == tail ); }
+int Circularbuffer::Full() {return (head==(tail+1)%cap);}
+int Circularbuffer::Length(void) const { return buffer_counter; }
---
+++ circularbuffer.h
@@ -0,0 +1,28 @@
+//
+//  circularbuffer.h
+//  Circularbuffer
+//
+//  Created by Treasure Nkawu on 16/04/2017.
+//  Copyright © 2017 Treasure Nkawu. All rights reserved.
+//
+
+#ifndef circularbuffer_h
+#define circularbuffer_h
+#define BUFFERSIZE 100
+
+class Circularbuffer{
+    private:
+        double *queue;
+        int cap,head,tail;
+    public:
+    int buffer_counter;
+    Circularbuffer(int s=BUFFERSIZE);
+    ~Circularbuffer();
+    void Insert(int v);
+    int Remove();
+    int Empty();
+    int Full();
+    int Length(void) const;
+};
+
+#endif /* circularbuffer_h */
