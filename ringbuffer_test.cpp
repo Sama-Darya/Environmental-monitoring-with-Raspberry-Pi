dc834d8ec3f8150e0206499001bd4daca83ce01a
Treasurenkawu16 <t.m.nkawu@hotmail.co.uk>
2017-04-08 23:59:47 +0100

    Create ringbuffer_test.cpp

---
+++ ringbuffer_test.cpp
@@ -0,0 +1,25 @@
+// This is program is to test the ringbuffer.h / ringbuffer.cpp
+// class Test is used inside main() to instantiate object for testing.
+//  main.cpp
+//  Ringbuffer_cpp
+//
+//  Created by Treasure Nkawu on 08/04/2017.
+//  Copyright © 2017 Treasure Nkawu. All rights reserved.
+//
+
+#include <iostream>
+
+#include <stdlib.h>
+
+#include "ringbuffer.h"
+
+class Test {
+}
+
+
+
+
+int main(void)
+{
+  return EXIT_SUCCESS;
+}
