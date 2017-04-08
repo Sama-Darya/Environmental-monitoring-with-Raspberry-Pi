c958c5e8d134852935c6f488092fb6e65e12cba2
Treasurenkawu16 <t.m.nkawu@hotmail.co.uk>
2017-04-08 23:46:35 +0100

    Create ringbuffer.h

---
+++ ringbuffer.h
@@ -0,0 +1,17 @@
+/*  ringbuffer.h
+*   A ring buffer or circular buffer is required to temporary store
+*   incoming data from the MCP3424 ADC. The old data is overwritten by new data
+*   when the buffer is full.
+*   Created by Treasure Nkawu on 08/04/2017.
+*   Copyright © 2017 Treasure Nkawu. All rights reserved.
+*/
+
+#ifndef ringbuffer_h
+#define ringbuffer_h
+#define BUFFER_SIZE 128
+/*buffer class defination begins below: */ 
+
+
+
+
+#endif /* end ringbuffer_h */
