a7a73990263e13edb88df0893cc024c7409037ca
Treasurenkawu16 <t.m.nkawu@hotmail.co.uk>
2017-04-08 23:55:41 +0100

    Create ringbuffer.cpp

---
+++ ringbuffer.cpp
@@ -0,0 +1,34 @@
+/*  ringbuffer.cpp
+*   The implimantation of the ringbuffer.h class
+*
+*   Created by Treasure Nkawu on 08/04/2017.
+*   Copyright © 2017 Treasure Nkawu. All rights reserved.
+*/
+
+#include <stdio.h>
+#include <iostream>
+
+#include <stdlib.h>
+#include "ringbuffer.h"
+
+//Error Handling
+
+
+
+
+//Constructor: called when instantiating an object
+
+
+
+
+
+
+
+
+
+//Destructor: clean-up or free memory used by the objects.
+
+
+
+
+
