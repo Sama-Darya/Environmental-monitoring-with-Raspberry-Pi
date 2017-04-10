2640187b09a8cd94b86ecd2f588b92b772694fd6
Treasurenkawu16 <t.m.nkawu@hotmail.co.uk>
2017-04-10 20:29:57 +0100

    Update ringbuffer.cpp

--- ringbuffer.cpp
+++ ringbuffer.cpp
@@ -12,23 +12,50 @@
 #include "ringbuffer.h"
 
 //Error Handling
-
+void QueueStuffed()
+{
+    std::cout << "Queue structure corrupted\n" << std::endl;
+    std::cout << "Read instructions next time" << std::endl;
+    exit(1);
+}
 
 
 
 //Constructor: called when instantiating an object
-
-
-
-
-
-
-
-
+Queue::Queue()
+{
+    fp = fg = fcount = 0;
+    //fp: input index
+    //fg: get index
+    //fcount: counter
+}
 
 //Destructor: clean-up or free memory used by the objects.
 
 
 
 
+//methods
+void Queue::Append(void* newitem)
+{
+    if(Full())
+        QueueStuffed();
+    fdata[fp] = newitem;
+    fp++;
+    if(fp == BUFFER_SIZE)
+        fp = 0;
+    fcount++;
+    return;
+}
 
+void *Queue::First(void)
+{
+    if(Empty())
+        QueueStuffed();
+    void* temp = fdata[fg];
+    fg++;
+    if(fg == BUFFER_SIZE)
+        fg = 0;
+    fcount--;
+    return temp;
+}
