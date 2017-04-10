451a94a66bfe4eb449b360268eaf260e04323aba
Treasurenkawu16 <t.m.nkawu@hotmail.co.uk>
2017-04-10 20:34:43 +0100

    Update ringbuffer_test.cpp

--- ringbuffer_test.cpp
+++ ringbuffer_test.cpp
@@ -1,11 +1,11 @@
 // This is program is to test the ringbuffer.h / ringbuffer.cpp
 // class Test is used inside main() to instantiate object for testing.
-//  main.cpp
 //  Ringbuffer_cpp
 //
 //  Created by Treasure Nkawu on 08/04/2017.
 //  Copyright © 2017 Treasure Nkawu. All rights reserved.
-//
+//  This code was tested running on Xcode (Macbook) with ringbuffer.h and ringbuffer.cpp in
+// the same directory.
 
 #include <iostream>
 
